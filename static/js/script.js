6fbc73e1055363e9f8652aeb1da189cff23389f8
HinaKhan94 <hinakhan94.hk@gmail.com>
2023-11-06 12:00:01 +0000

    chore: fix alert messages

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,15 @@
 const myModal = new bootstrap.Modal(document.getElementById('request-service'));
 const OfferModal = new bootstrap.Modal(document.getElementById('offerModal'));
 
+/** Modal event listener function **/
 window.addEventListener('DOMContentLoaded', () => {
    myModal.show();
    OfferModal.show();
    console.log('modal clicked');
 });
 
+/** Button hide on details page
+ *  event listener function **/
 function hideRequestButton() {
    // 
    document.querySelector('#make-request-btn').style.display = 'none';
